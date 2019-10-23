a74399b25833a39862cb262e95d9d7cd6f7b5cf6
Sashok <koshchin.shura@mail.ru>
2019-10-23 14:43:56 +0300

    half_of_lessons

--- Lessons_Body.py
+++ Lessons_Body.py
@@ -11,11 +11,38 @@ class Lessons(QMainWindow,Ui_MainWindow):
     def __init__(self, MainWindow):
         super().__init__()
         self.setupUi(self)
-        self.pushButton_1.pressed.connect(self.openLesson)
+        self.pushButton_1.pressed.connect(self.openLesson_1)
+        self.pushButton_2.pressed.connect(self.openLesson_2)
+        self.pushButton_3.pressed.connect(self.openLesson_3)
+        self.pushButton_4.pressed.connect(self.openLesson_4)
+        self.pushButton_5.pressed.connect(self.openLesson_5)
+        self.pushButton_6.pressed.connect(self.openLesson_6)
+        self.pushButton_7.pressed.connect(self.openLesson_7)
+        self.pushButton_8.pressed.connect(self.openLesson_8)
+        self.pushButton_9.pressed.connect(self.openLesson_9)
+        self.pushButton_10.pressed.connect(self.openLesson_10)
         self.pushButton_back.pressed.connect(self.Back)
         self.MainWindow = MainWindow
-    def openLesson(self):
-        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\keyboard-shortcuts-windows.pdf')
+    def openLesson_1(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_1.pdf')
+    def openLesson_2(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_2.pdf')
+    def openLesson_3(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_3.pdf')
+    def openLesson_4(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_4.pdf')
+    def openLesson_5(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_5.pdf')
+    def openLesson_6(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_6.pdf')
+    def openLesson_7(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_7.pdf')
+    def openLesson_8(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_8.pdf')
+    def openLesson_9(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_9.pdf')
+    def openLesson_10(self):
+        wb.open_new(r'C:\Users\Меднов Александр\Desktop\Project\Lessons\lesson_10.pdf')
     def Back(self):
         self.MainWindow.show()
         self.hide()

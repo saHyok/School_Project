e38375f641006106d54c83e53e0e8df4a69e30aa
Sashok <koshchin.shura@mail.ru>
2019-12-14 10:36:13 +0300

    Starting to do testing part

--- Testing_body.py
+++ Testing_body.py
@@ -13,18 +13,29 @@ class Testing(QMainWindow,Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.NumLesson = NumLesson
+        self.options = []
+        self.answers = []
+        self.questions = []
+        self.ReadData()
         self.FillIn()
     def ReadData(self):
-        file = open(r'QuestionsAnswers'+r'\'+str(self.NumLesson)+r\'+'Optional_answers')
-        result = []
-        for i in range(0,5):
-            data = []
-            for line in file:
-                data.append(line)
-            result.append(data)
+        Num = str(self.NumLesson)
+        roote = r'QuestionsAnswers'+'\\'+Num+'\Optional_answers.txt'
+        file = open(roote)
+        for line in file:
+           self.options.append(line)
+        RightRoote = r'QuestionsAnswers'+'\\'+Num+'\Answers.txt'
+        file = open(RightRoote)
+        for line in file:
+            self.answers.append(line)
+        QuestionRoote = r'QuestionsAnswers'+'\\'+Num+'\Questions.txt'
+        file = open(QuestionRoote)
+        for line in file:
+            self.questions.append(line)
     def FillIn(self):
         _translate = QtCore.QCoreApplication.translate
-        self.pushButton_1.setText(_translate("testing", "Тест 1"))
-        self.pushButton_2.setText(_translate("testing", "Тест 2"))
-        self.pushButton_3.setText(_translate("testing", "Тест 3"))
-        self.pushButton_4.setText(_translate("testing", "Тест 4"))
+        self.pushButton_1.setText(_translate("testing", self.options[0]))
+        self.pushButton_2.setText(_translate("testing", self.options[1]))
+        self.pushButton_3.setText(_translate("testing", self.options[2]))
+        self.pushButton_4.setText(_translate("testing", self.options[3]))
+        self.textBrowser.setHtml(_translate("MainWindow", self.questions[0]))
